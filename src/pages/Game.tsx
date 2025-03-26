8ea4ac8b970def2acf8e22f6bb1f1df9c037ea12
lewan <quangle1113@gmail.com>
2025-03-27 00:18:48 +0100

    Init Page Game

---
+++ src/pages/Game.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Board from '../components/Board/Board';
+
+const Game: React.FC = () => {
+    return (
+        <div>
+            <Board />
+        </div>
+    );
+}
+
+export default Game;

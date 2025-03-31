5cee07ee9e175a94d01e6e0e8f53d6fa0ad5e2c6
lewan <quangle1113@gmail.com>
2025-03-31 22:38:53 +0200

    Update turn

--- src/components/Board/Square.tsx
+++ src/components/Board/Square.tsx
@@ -1,23 +1,36 @@
-import { Box } from "@mui/material"
+import { Alert, Box, Dialog } from "@mui/material"
 import { Player } from "../../@types/player";
 import PlayerShow from "../Player/PlayerShow";
 
 type SquareProps = {
     x: number,
     y: number,
-    players: {P1: Player, P2: Player},
+    players: { P1: Player, P2: Player },
     selectedPlayer: Player | null,
     onSelectPlayer: (player: Player) => void,
     onMovePlayer: (x: number, y: number) => void,
     isValidMove?: boolean,
     isGameStarted: boolean,
+    turn: "P1" | "P2"
 };
-const Square: React.FC<SquareProps> = ({x, y, players, selectedPlayer, onSelectPlayer, onMovePlayer, isValidMove, isGameStarted} : SquareProps) => {
+const Square: React.FC<SquareProps> = ({ x, y, players, selectedPlayer, onSelectPlayer, onMovePlayer, isValidMove, isGameStarted, turn }: SquareProps) => {
     const playerHere = Object.values(players).find(player => player.position?.x === x && player?.position.y === y);
     const isYourPlayer = playerHere?.isPlayer === true;
+    const isYourTurn = playerHere?.isPlayer && turn === "P1"
+
+    const onAction = () => {
+        if (!isGameStarted) {
+            alert("You need to click 'New Game' to start playing.");
+            return;
+        }
+        else{
+            isYourPlayer && isYourTurn ?
+            onSelectPlayer(playerHere) : (selectedPlayer && isValidMove && onMovePlayer(x, y))
+        }
+    }
     return (
         <Box
-            onClick={() => isGameStarted && isYourPlayer ? onSelectPlayer(playerHere) : (selectedPlayer && isValidMove && onMovePlayer(x, y))}
+            onClick={onAction}
             sx={{
                 width: "100%",
                 height: "100%",
@@ -25,7 +38,7 @@ const Square: React.FC<SquareProps> = ({x, y, players, selectedPlayer, onSelectP
                 border: "1px solid black",
                 borderRadius: "4px",
                 backgroundColor: isYourPlayer ? playerHere.color :
-                                  isValidMove ? "#90caf955" : "white",
+                    isValidMove ? "#90caf955" : "white",
                 cursor: isYourPlayer || isValidMove ? "pointer" : "default",
                 "&:hover": isYourPlayer || isValidMove ? {
                     backgroundColor: isValidMove ? "#90caf9" : "#f8bbd0"

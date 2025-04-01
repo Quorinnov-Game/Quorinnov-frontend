6616fb91c7dfa2f1475bdcaf9e563e60fd472cdc
lewan <quangle1113@gmail.com>
2025-04-02 01:46:49 +0200

    Init tuto

---
+++ src/pages/Tuto.tsx
@@ -0,0 +1,16 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import videoTuto from "../assets/videos/Tutoquoridor.mp4"
+
+const Tuto: React.FC = () => {
+    return (
+        <Box>
+            <Typography variant="h2" fontWeight="bold" mb={2} >Tutoriel</Typography>
+            <video controls width="70%" style={{ borderRadius: '8px' }}>
+                <source src={videoTuto} type="video/mp4"></source>
+            </video>
+        </Box>
+    )
+}
+
+export default Tuto;

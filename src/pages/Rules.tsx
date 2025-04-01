1bf65ee0758bd40759533dc4dcf400e023d66e4f
lewan <quangle1113@gmail.com>
2025-04-02 01:47:48 +0200

    Init rules component

---
+++ src/pages/Rules.tsx
@@ -0,0 +1,34 @@
+import { Box, Divider, Typography } from "@mui/material";
+import winImg from "../assets/img/Win.png"
+import blockImg from "../assets/img/Block.png"
+import emplacementImg from "../assets/img/Emplacement.png"
+const Rules: React.FC = () => {
+    return (
+        <Box
+            maxWidth="800px"
+            mx="auto"
+            p={4}
+        >
+            <Typography variant="h3" fontWeight="bold" gutterBottom>
+                Les règles de Quoridor
+            </Typography>
+
+            <Divider sx={{ mb: 2 }} />
+
+            {/* Section 1 */}
+                <Typography variant="h6" fontWeight="medium" mb={2}>1. Objective</Typography>
+                <img src={winImg} alt="Moving Pawn" width="70%" style={{ borderRadius: 8, marginBottom: 16 }} />
+
+            {/* Section 2 */}
+                <Typography variant="h6" fontWeight="medium" mb={2}>2. Moving Your Pawn</Typography>
+                <img src={emplacementImg} alt="Moving Pawn" width="70%" style={{ borderRadius: 8, marginBottom: 16 }} />
+
+            {/* Section 3 */}
+                <Typography variant="h6" fontWeight="medium" mb={2}>3. Placing Walls</Typography>
+                <img src={blockImg} alt="Placing Wall" width="70%" style={{ borderRadius: 8, marginBottom: 16 }} />
+
+        </Box>
+    );
+};
+
+export default Rules;

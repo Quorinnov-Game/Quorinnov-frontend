58317103746640bed5169ca0aeea2d49f4f038e8
lewan <quangle1113@gmail.com>
2025-04-01 11:11:19 +0200

    Rename

--- src/components/Controls/ControlPanel.tsx
+++ src/components/Controls/ControlPanel.tsx
@@ -10,8 +10,8 @@ const ControlPanel: React.FC<ControlPanelProps> = ({ onNewGame, onUndo, onRedo }
     return (
         <Box display="flex" gap={2} mb={2}>
             <Button variant="contained" color="primary" onClick={onNewGame}>Nouvelle partie</Button>
-            <Button variant="contained" color="success" onClick={onUndo}>Retour pion</Button>
-            <Button variant="contained" color="warning" onClick={onRedo}>Arrivé pion</Button>
+            <Button variant="contained" color="success" onClick={onUndo}>Undo</Button>
+            <Button variant="contained" color="warning" onClick={onRedo}>Redo</Button>
         </Box>
     );
 }

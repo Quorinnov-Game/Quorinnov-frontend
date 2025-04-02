de8f1bdc83b23e16454222c8af1ed9478ef36615
lewan <quangle1113@gmail.com>
2025-04-03 00:27:33 +0200

    Update blur the background

--- src/pages/Tuto.tsx
+++ src/pages/Tuto.tsx
@@ -4,12 +4,24 @@ import videoTuto from "../assets/videos/Tutoquoridor.mp4"
 
 const Tuto: React.FC = () => {
     return (
-        <Box>
-            <Typography variant="h2" fontWeight="bold" mb={2} >Tutoriel</Typography>
-            <video controls width="70%" style={{ borderRadius: '8px' }}>
-                <source src={videoTuto} type="video/mp4"></source>
-            </video>
-        </Box>
+        <>
+            <Box
+                sx={{
+                    position: 'absolute',
+                    inset: 0,
+                    backdropFilter: 'blur(8px)',
+                    WebkitBackdropFilter: 'blur(8px)',
+                    backgroundColor: 'rgba(0,0,0,0.2)',
+                    zIndex: -1,
+                }}
+            />
+            <Box>
+                <Typography variant="h2" fontWeight="bold" mb={2} >Tutoriel</Typography>
+                <video controls width="70%" style={{ borderRadius: '8px' }}>
+                    <source src={videoTuto} type="video/mp4"></source>
+                </video>
+            </Box>
+        </>
     )
 }
 

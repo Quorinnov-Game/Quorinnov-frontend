c7d9e8785deb1aa7ef08c437df43006dfc0073a3
lewan <quangle1113@gmail.com>
2025-04-03 23:08:04 +0200

    Update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
             maxWidth="sm"
             sx={{ textAlign: "center", py: 5 }}
         >
-            <Box
+            {/* <Box
                 component="img"
                 src={Logo}
                 alt="Quoridor Logo"
@@ -21,11 +21,7 @@ const Home: React.FC = () => {
                     borderRadius: 2,
                     boxShadow: "0 4px 12px rgba(0,0,0,0.2)",
                 }}
-            />
-
-            <Typography variant="h3" fontWeight="bold" gutterBottom>
-                Quoridor Game
-            </Typography>
+            /> */}
 
             <Box display="flex" flexDirection="column" gap={2} alignItems="center" mt={4}>
                 <Button fullWidth variant="contained" color="primary" size="medium" onClick={() => navigate("/game")}>Jouer</Button>
